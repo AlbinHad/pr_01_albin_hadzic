dff98364d6b4788222c9fb9cabc0c464fc8765af
AlbinHad <2465302@cmontmorency.qc.ca>
2025-12-14 15:07:38 -0500

    commit

--- tp4/tp4/albin_hadzic_tp03/scenes/objets/ennemi.gd
+++ tp4/tp4/albin_hadzic_tp03/scenes/objets/ennemi.gd
@@ -1,25 +1,26 @@
 extends RigidBody2D
 
-@onready var game_manager: Node = %gamemanager
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var game_manager = %gamemanager
 
+func _on_area_2d_body_entered(body):
+	if body.name == "CharacterBody2D":
+		var y_delta = position.y - body.position.y
+		var x_delta = body.position.x - position.x
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+		# Stomp (par le dessus)
+		if y_delta > 30:
+			call_deferred("die")
+			body.call_deferred("bounce_up")
 
-
-func _on_area_2d_body_entered(body: Node2D) -> void:
-	if (body.name == "CharacterBody2D"):
-		var y_delta = position. y - body.position.y
-		if (y_delta > 30):
-			print ("Destroy enemy")
-			queue_free()
-			body.velocity.y = -400
-
+		# Collision sur le côté
 		else:
-			print("Decrease player health")
-		
 			game_manager.decrease_health()
+
+			if x_delta > 0:
+				body.call_deferred("jump_side", 500)
+			else:
+				body.call_deferred("jump_side", -500)
+
+
+func die():
+	queue_free()
--- tp4/tp4/albin_hadzic_tp03/scenes/objets/personnage_principal.gd
+++ tp4/tp4/albin_hadzic_tp03/scenes/objets/personnage_principal.gd
@@ -1,47 +1,53 @@
 extends CharacterBody2D
-@onready var sprite_2d: = $Sprite2D
 
+@onready var sprite_2d = $Sprite2D
+@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
 
 var SPEED = 400.0
 const JUMP_VELOCITY = -900.0
-@onready var sfx_jump: AudioStreamPlayer2D = $sfx_jump
-
-
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+
 func _physics_process(delta: float) -> void:
-	
-	if velocity.x > 1 or velocity.x < -1:
+
+	# Gravité
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	# Déplacement horizontal (inchangé)
+	var direction = Input.get_axis("left", "right")
+	if direction != 0.0:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, 12)
+
+	# Animations (inchangé)
+	if not is_on_floor():
+		sprite_2d.animation = "jumping"
+	elif abs(velocity.x) > 1:
 		sprite_2d.animation = "running"
 	else:
 		sprite_2d.animation = "default"
 
-	
-	if not is_on_floor():
-		velocity.y += gravity * delta
-		sprite_2d.animation = "jumping"
-
-	# Gestion du saut
+	# Saut (inchangé)
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 		sfx_jump.play()
 
-	
-	var direction = Input.get_axis("left", "right")
+	move_and_slide()
 
-	
-	if direction != 0.0:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0,12)
-
-	
-	move_and_slide() 
-	var isLeft = velocity.x<0
-	sprite_2d.flip_h = isLeft
+	sprite_2d.flip_h = velocity.x < 0
 
 
+# 🔥 REBOND SUR LE CÔTÉ (corrigé, sans input)
+func jump_side(x):
+	velocity.y = JUMP_VELOCITY
+	velocity.x = x
+
+
+# 🍉 BONUS RAMASSÉ (inchangé)
 func _on_body_entered(body):
-	if body.name == "CharacterBody2D": # nom de ton joueur
-		body.SPEED = 700.0
+	if body.name == "CharacterBody2D":
+		SPEED = 700.0
 		print("🍉 Melon ramassé ! Vitesse augmentée.")
 		queue_free()
