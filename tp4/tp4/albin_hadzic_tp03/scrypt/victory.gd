d49fef63a5fb827723668da3fba2bac6ecadb521
AlbinHad <2465302@cmontmorency.qc.ca>
2025-12-14 19:58:22 -0500

    committtttttt

--- tp4/tp4/albin_hadzic_tp03/scrypt/victory.gd
+++ tp4/tp4/albin_hadzic_tp03/scrypt/victory.gd
@@ -3,4 +3,15 @@ extends Control
 func _on_button_pressed():
 	print("PRESSED OK")
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/menu/main_menu.tscn")
+	get_tree().call_deferred(
+		"change_scene_to_file",
+		"res://scenes/menu/main_menu.tscn"
+	)
+
+func _on_button_2_pressed():
+	print("CREDITS OK")
+	get_tree().paused = false
+	get_tree().call_deferred(
+		"change_scene_to_file",
+		"res://scenes/menu/credit.tscn"
+	)
